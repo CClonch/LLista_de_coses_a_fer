70c61936e7d95189096f709c9e99e3c7d25ebf14
adri <h>
2022-08-04 16:55:08 +0200

    Initial commit

---
+++ app/src/main/java/lab/clnx/llistadecosesafer/MainActivity.kt
@@ -0,0 +1,11 @@
+package lab.clnx.llistadecosesafer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
